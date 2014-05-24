a8bd4ab4a50f90bf1804b662671bbdbc61200ccb
Yoav Givati <hello@yoavgivati.com>
2014-05-24 13:06:53 -0400

    highf.in compatability

--- web/salmon/tail.js
+++ web/salmon/tail.js
@@ -150,7 +150,7 @@ tail = {
     }
 }
 
-require('./node_modules/shelljs/global');
+//require('./node_modules/shelljs/global');
 var path = require('path');
 
 // define app paths, passing in the base project directory
